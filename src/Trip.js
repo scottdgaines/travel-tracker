62636effbaa892dbb1fbb3bdab6c932c597fe1ac
Scott Gaines <scottdgaines@gmail.com>
2022-09-22 11:31:11 -0600

    Add code and methods to pass all tests

---
+++ src/Trip.js
@@ -0,0 +1,27 @@
+class Trip {
+    constructor(travelerID, newTripData) {
+        this.id = 1,
+        this.userID = travelerID,
+        this.destinationID = newTripData.destinationName,
+        this.travelers = newTripData.travelers,
+        this.date = newTripData.date,
+        this.duration = newTripData.duration,
+        this.status = 'pending',
+        this.suggestedActivities = []
+    };
+
+    findDestinationID(destinations, destinationName) {
+        const destinationID = destinations.find(destination => {
+            return destination.destination === destinationName
+        });
+        
+        if (destinationID === undefined) {
+            this.destinationID = null;
+            return 'Something went wrong, please try again later';
+        } else 
+            return this.destinationID = destinationID.id;
+        };
+        
+}
+
+export default Trip;
