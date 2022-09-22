b21d8fdf194d22afc0a3d721af1c810f0865181e
Scott Gaines <scottdgaines@gmail.com>
2022-09-22 14:39:14 -0600

    Add functions to calculate cost of trip

--- src/Trip.js
+++ src/Trip.js
@@ -21,6 +21,31 @@ class Trip {
         } else 
             return this.destinationID = destinationID.id;
         };
+
+    retrieveDestinationData(destinations) {
+        this.findDestinationID(destinations, this.destinationID)
+
+        return destinations.find(destination => {
+            return destination.id === this.destinationID
+        }) 
+    }
+
+    calculateCosts(destinations) {
+        const destinationData = this.retrieveDestinationData(destinations)
+
+        const lodgingPerDay = destinationData.estimatedLodgingCostPerDay;
+        const flightPerPerson = destinationData.estimatedFlightCostPerPerson;
+
+        const totalLodging = lodgingPerDay * this.duration
+        const totalFlights = flightPerPerson * this.travelers
+
+        const totalTrip = totalLodging + totalFlights
+
+        const agentFee = totalTrip * .10
+
+        return `$${totalTrip + agentFee}`
+
+    }
         
 }
 
