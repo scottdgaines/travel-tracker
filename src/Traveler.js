6a95eb8daf089d83dc1c4b33507e2ea2d2f157bb
Scott Gaines <scottdgaines@gmail.com>
2022-09-22 18:31:14 -0600

    Added code to calculcate total spent, return a year's trip

--- src/Traveler.js
+++ src/Traveler.js
@@ -29,19 +29,33 @@ class Traveler {
         });
     };
 
-    createNewTrip(trips, destinations, newTripData) {
-        const tripID = this.generateTripID(trips)
-        const destinationID = this.updateDestinationID(destinations, newTripData)
+    returnTripsPerYear(tripsData) {
+        let currentYear = String(new Date().getFullYear());
 
+        return tripsData.filter(trip => {
+            return trip.date.slice(0, 4) === currentYear && trip.userID === this.id;
+        });
+    };
+
+    createNewTrip(trips, destinations, newTripData) {
+        const tripID = this.generateTripID(trips);
+        const destinationID = this.updateDestinationID(destinations, newTripData);
         const newTrip = new Trip(tripID, this.id, newTripData, destinationID);
 
-        return newTrip
+        return newTrip;
     };
 
     calculateTotalSpent(trips, destinations) {
-        const previousTrips = this.returnPreviousTrips(trips);
+        const yearsTrips = this.returnTripsPerYear(trips);
 
-    }
+        const tripTotals = yearsTrips.map(trip => {
+            return this.calculateCostsPerTrip(destinations, trip);
+        });
+
+        return tripTotals.reduce((acc, total) => {
+            return acc += total;
+        }, 0);
+    };
 
     //Helper Functions
     returnCurrentDate() {
@@ -56,21 +70,42 @@ class Traveler {
     };
 
     generateTripID(trips) {
-        return trips.length + 1
+        return trips.length + 1;
     };
 
     updateDestinationID(destinations, newTripData) {
         const destinationID = destinations.find(destination => {
-            return destination.destination === newTripData.destinationName
+            return destination.destination === newTripData.destinationName;
         });
         
         if (destinationID === undefined) {
-            this.destinationID = null;
-            return 'Something went wrong, please try again later';
+            alert('Something went wrong, please try again later');
+            return this.destinationID = null; 
         } else {
             return this.destinationID = destinationID.id;
         };
-    }
+    };
+
+    calculateCostsPerTrip(destinations, trip) {
+        const destinationData = this.retrieveDestinationData(destinations, trip.destinationID);
+       
+        const lodgingPerDay = destinationData.estimatedLodgingCostPerDay;
+        const flightPerPerson = destinationData.estimatedFlightCostPerPerson;
+
+        const totalLodging = lodgingPerDay * trip.duration;
+        const totalFlights = flightPerPerson * trip.travelers;
+
+        const totalTrip = totalLodging + totalFlights;
+        const agentFee = totalTrip * .10;
+        
+        return totalTrip + agentFee;
+    };
+
+    retrieveDestinationData(destinations, destinationID) {
+        return destinations.find(destination => {
+            return destination.id === destinationID;
+        });
+    };
 };
 
 export default Traveler;
