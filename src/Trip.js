bcdc00b269b8617e593658bcd614efe00ed0db32
Scott Gaines <scottdgaines@gmail.com>
2022-09-22 18:32:46 -0600

    Refactor methods:

--- src/Trip.js
+++ src/Trip.js
@@ -11,26 +11,26 @@ class Trip {
     };
 
     calculateCosts(destinations) {
-        const destinationData = this.retrieveDestinationData(destinations)
+        const destinationData = this.retrieveDestinationData(destinations);
 
         const lodgingPerDay = destinationData.estimatedLodgingCostPerDay;
         const flightPerPerson = destinationData.estimatedFlightCostPerPerson;
 
-        const totalLodging = lodgingPerDay * this.duration
-        const totalFlights = flightPerPerson * this.travelers
+        const totalLodging = lodgingPerDay * this.duration;
+        const totalFlights = flightPerPerson * this.travelers;
 
-        const totalTrip = totalLodging + totalFlights
+        const totalTrip = totalLodging + totalFlights;
 
-        const agentFee = totalTrip * .10
+        const agentFee = totalTrip * .10;
 
-        return `$${totalTrip + agentFee}`
+        return totalTrip + agentFee;
     }
 
     retrieveDestinationData(destinations) {
         return destinations.find(destination => {
-            return destination.id === this.destinationID
-        }) 
-    }
-}
+            return destination.id === this.destinationID;
+        });
+    };
+};
 
 export default Trip;
