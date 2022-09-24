461b5202fd93bff8aa3f6cebaf75d54791a97c94
Scott Gaines <scottdgaines@gmail.com>
2022-09-23 20:22:53 -0600

    Add POST and error handling

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,4 +4,27 @@ function fetchData(dataCategory) {
         .then(data => data)
 }
 
-export default fetchData 
+function fetchPost(newTripData) {
+    return fetch('http://localhost:3001/api/v1/trips', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(newTripData)
+    })
+        .then(response => handleErrors(response))
+        .then(response => response.json())
+        .catch(err => showErrorMessage())
+}
+
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    } else {
+        return response
+    }
+ }
+
+function showErrorMessage() {
+    errorMessage.classList.remove('hidden')
+}
+
+export { fetchData, fetchPost }
