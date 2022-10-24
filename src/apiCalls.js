3c01cd5b26bea86024352d7304e6d053223d3b6b
Scott Gaines <scottdgaines@gmail.com>
2022-10-24 17:44:57 -0600

    Update endpoints

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,13 @@
 import { updateData, hide } from './scripts.js'
 
 function fetchData(dataCategory) {
-    return fetch(`http://localhost:3001/api/v1/${dataCategory}`)
+    return fetch(`https://travel-tracker-o2io4eikw-scottdgaines.vercel.app/api/v1/${dataCategory}`)
         .then(response => response.json())
         .then(data => data);
 };
 
 function fetchPost(newTripData) {
-    return fetch('http://localhost:3001/api/v1/trips', {
+    return fetch('https://travel-tracker-o2io4eikw-scottdgaines.vercel.app/api/v1/trips', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(newTripData)
